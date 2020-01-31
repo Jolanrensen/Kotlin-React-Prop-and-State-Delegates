fcb25fce372c17fbc5380b02eafa36e60de86df4
Jolanrensen <j.j.r.rensen@student.tue.nl>
2020-01-31 11:14:38 +0100

    initial

---
+++ delegates/RPureComponent.kt
@@ -0,0 +1,34 @@
+package delegates
+
+import kotlinext.js.jsObject
+import react.PureComponent
+import react.RBuilder
+import react.RProps
+import react.RState
+import react.buildElements
+
+/**
+ * Waiting for pull request: https://github.com/JetBrains/kotlin-wrappers/pull/142
+ * */
+
+abstract class RPureComponent<P : RProps, S : RState> : PureComponent<P, S> {
+    constructor() : super() {
+        state = jsObject { init() }
+    }
+
+    constructor(props: P) : super(props) {
+        state = jsObject { init(props) }
+    }
+
+    open fun S.init() {}
+    // if you use this method, don't forget to pass props to the constructor first
+    open fun S.init(props: P) {}
+
+    fun RBuilder.children() {
+        props.children()
+    }
+
+    abstract fun RBuilder.render()
+
+    override fun render() = buildElements { render() }
+}
